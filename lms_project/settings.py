e1c3fc8ab90897f3390c2579ecbae1451be062c0
PrachiAhire <prachiahire75@gmail.com>
2026-09-05 00:57:44 +0530

    First

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6dq=q=)nxlyr$r@*ch#g!ax62+mcgiz(kkm1rp$w_21qs7)tj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
