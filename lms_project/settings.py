065d65137e1447026ce51302633e82a7a1417fa1
PrachiAhire <prachiahire75@gmail.com>
2026-09-05 10:17:04 +0530

    Configure project for Railway deployment (gunicorn, whitenoise, nixpacks)

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6dq=q=)nxlyr$r@*ch#g!ax62+mcgiz(kkm1rp$w_21qs7)tj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email
